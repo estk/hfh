79557c127b0642cd652bf7b44f4f33693d668cb1
Evan Simmons <esims89@gmail.com>
2013-10-14 16:46:13 -0700

    Added some logic for projects.

--- client/views/pages/projects.js
+++ client/views/pages/projects.js
@@ -1,5 +1,38 @@
 Template.projects.helpers({
   projects: function () {
-    return [];
+    return [
+      {
+        thumbnail: "http://hello.com",
+        url: "http://hello.com",
+        subphotos: [
+          {
+            url: "http://hello.com",
+            description: "Blah"
+          }
+        ]
+      }
+    ];
   }
 });
+
+Template.carousel.rendered = function () {
+  // get photos
+  var cb = function (err, res) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    // XXX: Correct?
+    var photos = EJSON.parse(res.content).photos.photo;
+    _.each(photos, function (p) {
+      Photos.insert(p);
+    })
+    console.log("Photos added!");
+  };
+  // XXX: Change to correct tags
+  var tags = "homan,blah,foo"
+  Flickr.getWithTags(tags, cb)
+
+  // XXX: Initialize fancybox?
+
+}
